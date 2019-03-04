57ab44f6221c909bbf83f2feb022d40a3bc32419
Juan Cespedes <cespedes@thehackers.org>
2019-03-04 17:35:36 +0100

    added simple KNX client to show all received packets

--- main.go
+++ main.go
@@ -4,9 +4,12 @@ import (
 	"log"
 	"fmt"
 	"flag"
+	"strings"
 	"net/http"
-//	"github.com/vapourismo/knx-go/knx"
+	"github.com/vapourismo/knx-go/knx"
 )
+
+const DefaultKNXPort = 3671
 
 func webserver(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "this is the web server")
@@ -14,7 +17,20 @@ func webserver(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	webport := flag.Int("port", 8001, "port to listen for incoming connections")
+	knxrouter := flag.String("knx", "", "address of KNX router")
 	flag.Parse()
+
+	if !strings.Contains(*knxrouter, ":") {
+			*knxrouter = fmt.Sprintf("%s:%d", *knxrouter, DefaultKNXPort)
+	}
+	client, err := knx.NewGroupTunnel(*knxrouter, knx.DefaultTunnelConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Close()
+	for msg := range client.Inbound() {
+		log.Printf("KNX: %+v", msg)
+	}
 
 	http.HandleFunc("/", webserver)
 	log.Printf("Starting web server on port %d...", *webport)
