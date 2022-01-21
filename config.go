20a73f48561911664261ccb9a8d59f604255f38f
Juan Cespedes <juan.cespedes@imdea.org>
2022-01-21 18:08:18 +0100

    more fixes; permanent storage in disk for last values

--- config.go
+++ config.go
@@ -12,12 +12,25 @@ import (
 
 /* Syntax of KNXweb config file:
 
+gateway 192.168.1.11
+	...
+
 device 1.1.10 myroom.i/o
 	...
 
 address 2/5/7 1.009 myroom.door
 	...
 */
+type addrNameType struct {
+	Name string
+	Type dpt.DatapointValue
+}
+
+type Config struct {
+	Gateways  []string
+	Devices   map[cemi.IndividualAddr]string
+	Addresses map[cemi.GroupAddr]addrNameType
+}
 
 type UnknownDPT []byte
 
@@ -40,10 +53,13 @@ func (d UnknownDPT) Unit() string {
 	return ""
 }
 
-func ReadConfig(filename string) error {
+func ReadConfig(filename string) (*Config, error) {
+	var c Config
+	c.Devices = make(map[cemi.IndividualAddr]string)
+	c.Addresses = make(map[cemi.GroupAddr]addrNameType)
 	f, err := os.Open(filename)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	s := bufio.NewScanner(f)
 	lineNum := 0
@@ -60,34 +76,39 @@ func ReadConfig(filename string) error {
 		}
 		tokens := strings.Fields(line)
 		switch tokens[0] {
+		case "gateway":
+			if len(tokens) != 2 {
+				return nil, fmt.Errorf("Syntax error in %s line %d\n", filename, lineNum)
+			}
+			c.Gateways = append(c.Gateways, tokens[1])
 		case "device":
 			if len(tokens) != 3 {
-				return fmt.Errorf("%s line %d: %s\n", filename, lineNum, line)
+				return nil, fmt.Errorf("Syntax error in %s line %d\n", filename, lineNum)
 			}
 			// fmt.Printf("line %d: new device: %v\n", lineNum, tokens)
 			addr, err := cemi.NewIndividualAddrString(tokens[1])
 			if err != nil {
-				return fmt.Errorf("%s line %d: %w", filename, lineNum, err)
+				return nil, fmt.Errorf("Error in %s line %d: %w", filename, lineNum, err)
 			}
-			devices[addr] = tokens[2]
+			c.Devices[addr] = tokens[2]
 		case "address":
 			if len(tokens) != 4 {
-				return fmt.Errorf("Syntax error in line %d: %s\n", lineNum, line)
+				return nil, fmt.Errorf("Syntax error in %s line %d\n", filename, lineNum)
 			}
 			// fmt.Printf("line %d: new address: %v\n", lineNum, tokens)
 			addr, err := cemi.NewGroupAddrString(tokens[1])
 			if err != nil {
-				return fmt.Errorf("%s line %d: %w", filename, lineNum, err)
+				return nil, fmt.Errorf("Error in %s line %d: %w", filename, lineNum, err)
 			}
 			dp, ok := dpt.Produce(tokens[2])
 			if !ok {
 				fmt.Printf("Warning: %s line %d: unknown type %v\n", filename, lineNum, tokens[2])
 				dp = new(UnknownDPT)
 			}
-			addresses[addr] = addrNameType{Name: tokens[3], Type: dp}
+			c.Addresses[addr] = addrNameType{Name: tokens[3], Type: dp}
 		default:
-			return fmt.Errorf("Syntax error in line %d: %s\n", lineNum, line)
+			return nil, fmt.Errorf("Syntax error in %s line %d: unrecognized token %s\n", filename, lineNum, tokens[0])
 		}
 	}
-	return nil
+	return &c, nil
 }
--- main.go
+++ main.go
@@ -14,7 +14,6 @@ import (
 
 	"github.com/vapourismo/knx-go/knx"
 	"github.com/vapourismo/knx-go/knx/cemi"
-	"github.com/vapourismo/knx-go/knx/dpt"
 )
 
 const (
@@ -22,170 +21,182 @@ const (
 	KNXTimeout     = 60 // no messages in several seconds: probable error in connection
 )
 
+var config *Config
+
+type Server struct {
+	Debug bool
+
+	Mutex        sync.Mutex
+	Messages     []knxMsg
+	Values       map[cemi.GroupAddr]knxMsg
+	SortedValues []cemi.GroupAddr
+}
+
+func (k knxMsg) String() string {
+	str := k.When.Format("2006-01-02 15:04:05")
+	switch k.Event.Command {
+	case knx.GroupRead:
+		str += " read:"
+	case knx.GroupResponse:
+		str += " response:"
+	case knx.GroupWrite:
+		str += " write:"
+	default:
+		str += " ???:"
+	}
+	str += " " + k.Event.Source.String() + " " + k.Event.Destination.String() + "=" + fmt.Sprint(k.Event.Data)
+	if dev, ok := config.Devices[k.Event.Source]; ok {
+		str += " " + dev
+	}
+	if nt, ok := config.Addresses[k.Event.Destination]; ok {
+		t := nt.Type
+		if err := t.Unpack(k.Event.Data); err != nil {
+			fmt.Printf("Network: Error parsing %v for %v\n", k.Event.Data, k.Event.Destination)
+		} else {
+			str += " " + nt.Name + "=" + fmt.Sprint(t)
+		}
+	}
+	return str
+}
+
 type knxMsg struct {
 	When  time.Time
 	Event knx.GroupEvent
 }
 
-type addrNameType struct {
-	Name string
-	Type dpt.DatapointValue
-}
-
-var devices = make(map[cemi.IndividualAddr]string)
-var addresses = make(map[cemi.GroupAddr]addrNameType)
-
-func (k knxMsg) String() string {
-	s := k.When.Format("2006-01-02 15:04:05")
-	switch k.Event.Command {
-	case knx.GroupRead:
-		s += " read:"
-	case knx.GroupResponse:
-		s += " response:"
-	case knx.GroupWrite:
-		s += " write:"
-	default:
-		s += " ???:"
-	}
-	s += " " + k.Event.Source.String() + " " + k.Event.Destination.String() + "=" + fmt.Sprint(k.Event.Data)
-	if str, ok := devices[k.Event.Source]; ok {
-		s += " " + str
-	}
-	if nt, ok := addresses[k.Event.Destination]; ok {
-		t := nt.Type
-		if err := t.Unpack(k.Event.Data); err != nil {
-			fmt.Printf("Network: Error parsing %v for %v\n", k.Event.Data, k.Event.Destination)
-		} else {
-			s += " " + nt.Name + "=" + fmt.Sprint(t)
-		}
-	}
-	return s
-}
-
-var mutex sync.Mutex
-var messages []knxMsg
-var values = map[cemi.GroupAddr]knxMsg{}
-var sortedValues []cemi.GroupAddr
-
-func knxNewMessage(event knx.GroupEvent) {
+func (s *Server) knxNewMessage(event knx.GroupEvent) {
 	msg := knxMsg{When: time.Now(), Event: event}
 	Log(msg)
-	mutex.Lock()
-	messages = append(messages, msg)
-	if _, ok := values[event.Destination]; !ok {
+	s.Mutex.Lock()
+	s.Messages = append(s.Messages, msg)
+	if _, ok := s.Values[event.Destination]; !ok {
 		// this destination has not been seen yet
-		log.Printf("New destination group addr: %v", event.Destination)
-		sortedValues = append(sortedValues, event.Destination)
-		sort.Slice(sortedValues, func(i, j int) bool { return sortedValues[i] < sortedValues[j] })
+		if s.Debug {
+			log.Printf("New destination group addr: %v", event.Destination)
+		}
+		s.SortedValues = append(s.SortedValues, event.Destination)
+		sort.Slice(s.SortedValues, func(i, j int) bool { return s.SortedValues[i] < s.SortedValues[j] })
 	}
-	values[event.Destination] = msg
-	mutex.Unlock()
+	s.Values[event.Destination] = msg
+	s.Mutex.Unlock()
 	fmt.Println(msg)
 	// log.Printf("KNX: %+v", event)
 	// b, _ := json.Marshal(event)
 	// log.Printf("JSON: %v", string(b))
 }
 
-func knxGetMessages(knxrouter string) {
-	if !strings.Contains(knxrouter, ":") {
-		knxrouter = fmt.Sprintf("%s:%d", knxrouter, KNXDefaultPort)
+func (s *Server) knxGetMessages() {
+	for i, gw := range config.Gateways {
+		if !strings.Contains(gw, ":") {
+			config.Gateways[i] = fmt.Sprintf("%s:%d", gw, KNXDefaultPort)
+		}
+	}
+	if s.Debug {
+		fmt.Printf("gateways: %v\n", config.Gateways)
 	}
 
-	for {
-		log.Printf("Stablishing connection to KNX router %s...\n", knxrouter)
+	for _, gw := range config.Gateways {
+		go func(gw string) {
+			for {
+				log.Printf("Stablishing connection to KNX gateway %s...\n", gw)
 
-		client, err := knx.NewGroupTunnel(knxrouter, knx.DefaultTunnelConfig)
-		if err != nil {
-			log.Fatalf("knx.NewGroupTunnel: %s", err.Error())
-		}
-		defer client.Close()
-
-		knxChan := client.Inbound()
-
-	innerLoop:
-		for {
-			select {
-			case <-time.After(KNXTimeout * time.Second):
-				log.Printf("timeout (%d seconds)", KNXTimeout)
-				break innerLoop
-			case event, ok := <-knxChan:
-				if !ok {
-					log.Printf("not ok")
-					break innerLoop
+				client, err := knx.NewGroupTunnel(gw, knx.DefaultTunnelConfig)
+				if err != nil {
+					log.Printf("knx.NewGroupTunnel (%s): %s", gw, err.Error())
+					log.Printf("Sleeping %d seconds...", KNXTimeout)
+					time.Sleep(KNXTimeout * time.Second)
+					continue
 				}
-				knxNewMessage(event)
+				defer client.Close()
+
+				knxChan := client.Inbound()
+
+			innerLoop:
+				for {
+					select {
+					case <-time.After(KNXTimeout * time.Second):
+						log.Printf("timeout (%d seconds)", KNXTimeout)
+						break innerLoop
+					case event, ok := <-knxChan:
+						if !ok {
+							log.Printf("Error reading from KNX channel")
+							break innerLoop
+						}
+						s.knxNewMessage(event)
+					}
+				}
+				client.Close()
+				time.Sleep(time.Second)
 			}
-		}
-		client.Close()
-		time.Sleep(time.Second)
+		}(gw)
 	}
 }
 
-func webRoot(w http.ResponseWriter, r *http.Request) {
+func (s *Server) webRoot(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "ROOT: %s\n", r.URL)
 }
 
-func getAddrs(s string) []cemi.GroupAddr {
+func (s *Server) getAddrs(str string) []cemi.GroupAddr {
 	var a, b, c uint8
 	var result []cemi.GroupAddr
-	i, _ := fmt.Sscanf(s, "%d/%d/%d", &a, &b, &c)
+	i, _ := fmt.Sscanf(str, "%d/%d/%d", &a, &b, &c)
 	if i == 3 {
 		return append(result, cemi.NewGroupAddr3(a, b, c))
 	}
-	for key, val := range addresses {
-		if s == val.Name {
+	for key, val := range config.Addresses {
+		if str == val.Name {
 			return append(result, key)
 		}
-		if strings.HasPrefix(val.Name, s+"/") {
+		if strings.HasPrefix(val.Name, str+"/") {
 			result = append(result, key)
 		}
 	}
 	return result
 }
 
-func webGet(w http.ResponseWriter, r *http.Request) {
+func (s *Server) webGet(w http.ResponseWriter, r *http.Request) {
 	path := r.URL.Path[5:]
 	if path == "latest" {
-		mutex.Lock()
-		if len(messages) == 0 {
-			mutex.Unlock()
+		s.Mutex.Lock()
+		if len(s.Messages) == 0 {
+			s.Mutex.Unlock()
 			return
 		}
-		msg := messages[len(messages)-1]
-		mutex.Unlock()
+		msg := s.Messages[len(s.Messages)-1]
+		s.Mutex.Unlock()
 		fmt.Fprintf(w, "%+v\n", msg)
 	} else if path == "all" {
-		mutex.Lock()
-		for i := range sortedValues {
-			fmt.Fprintf(w, "%+v\n", values[sortedValues[i]])
+		s.Mutex.Lock()
+		for i := range s.SortedValues {
+			fmt.Fprintf(w, "%+v\n", s.Values[s.SortedValues[i]])
 		}
-		mutex.Unlock()
+		s.Mutex.Unlock()
 	} else if strings.HasPrefix(path, "all/") {
-		addrs := getAddrs(path[4:])
+		addrs := s.getAddrs(path[4:])
 		if len(addrs) == 0 {
 			http.Error(w, "404 Not Found", http.StatusBadRequest)
 			return
 		}
-		mutex.Lock()
-		for _, m := range messages {
+		s.Mutex.Lock()
+		for _, m := range s.Messages {
 			for _, a := range addrs {
 				if m.Event.Destination == a {
 					fmt.Fprintf(w, "%+v\n", m)
 				}
 			}
 		}
-		mutex.Unlock()
+		s.Mutex.Unlock()
 	} else if strings.HasPrefix(path, "raw/") {
-		addrs := getAddrs(path[4:])
+		addrs := s.getAddrs(path[4:])
 		if len(addrs) == 0 {
 			http.Error(w, "404 Not Found", http.StatusBadRequest)
 			return
 		}
 
-		mutex.Lock()
+		s.Mutex.Lock()
 		for _, addr := range addrs {
-			if msg, ok := values[addr]; ok {
-				if nt, ok := addresses[addr]; ok {
+			if msg, ok := s.Values[addr]; ok {
+				if nt, ok := config.Addresses[addr]; ok {
 					t := nt.Type
 					if err := t.Unpack(msg.Event.Data); err != nil {
 						fmt.Fprintf(w, "Error parsing %v for %v\n", msg.Event.Data, msg.Event.Destination)
@@ -198,55 +209,98 @@ func webGet(w http.ResponseWriter, r *http.Request) {
 				}
 			}
 		}
-		mutex.Unlock()
+		s.Mutex.Unlock()
 	} else {
-		addrs := getAddrs(path)
+		addrs := s.getAddrs(path)
 		if len(addrs) == 0 {
 			http.Error(w, "404 Not Found", http.StatusBadRequest)
 			return
 		}
-		mutex.Lock()
+		s.Mutex.Lock()
 		for _, addr := range addrs {
-			if msg, ok := values[addr]; ok {
+			if msg, ok := s.Values[addr]; ok {
 				fmt.Fprintf(w, "%+v\n", msg)
 			}
 		}
-		mutex.Unlock()
+		s.Mutex.Unlock()
 	}
 }
 
-func webSet(w http.ResponseWriter, r *http.Request) {
+func (s *Server) webSet(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "SET: %s", r.URL)
 }
 
 func main() {
+	var s Server
+	s.Values = make(map[cemi.GroupAddr]knxMsg)
 	debug := flag.Bool("debug", false, "debugging info")
 	webport := flag.Int("port", 8001, "port to listen for incoming connections")
-	knxrouter := flag.String("knx", "", "address of KNX router")
 	logdir := flag.String("logdir", "", "directory where logs are stored")
 	flag.Parse()
+	if *debug {
+		s.Debug = true
+	}
 	if *logdir != "" {
 		logDir = *logdir
 		fmt.Printf("logdir = %s\n", logDir)
 	}
 
-	if *knxrouter == "" {
-		log.Fatal("No KNX router specified.  Please use option -knx")
-	}
-	if err := ReadConfig("knx.cfg"); err != nil {
+	var err error
+	config, err = ReadConfig("knx.cfg")
+	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
+	if len(config.Gateways) == 0 {
+		log.Fatal("No KNX gateway specified.  Please use \"gateway xx.xx.xx.xx\" in config file.")
+	}
 	if *debug {
-		fmt.Printf("devices: %v\n", devices)
-		fmt.Printf("addresses: %v\n", addresses)
+		fmt.Printf("gateways: %v\n", config.Gateways)
+		fmt.Printf("devices: %v\n", config.Devices)
+		fmt.Printf("addresses: %v\n", config.Addresses)
 	}
 
-	go knxGetMessages(*knxrouter)
+	func() {
+		// TODO: specify file location in config file
+		file, err := os.Open("status.json")
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		defer file.Close()
+		decoder := json.NewDecoder(file)
+		err = decoder.Decode(&s.Values)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		for key := range s.Values {
+			s.SortedValues = append(s.SortedValues, key)
+		}
+		sort.Slice(s.SortedValues, func(i, j int) bool { return s.SortedValues[i] < s.SortedValues[j] })
 
-	http.HandleFunc("/", webRoot)
-	http.HandleFunc("/get/", webGet)
-	http.HandleFunc("/set/", webSet)
+	}()
+
+	go s.knxGetMessages()
+	go func() {
+		for {
+			time.Sleep(30 * time.Second)
+			// TODO: create file atomically (race!)
+			// TODO: specify file location in config file
+			file, err := os.Create("status.json")
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			encoder := json.NewEncoder(file)
+			err = encoder.Encode(s.Values)
+			file.Close()
+		}
+	}()
+
+	http.HandleFunc("/", s.webRoot)
+	http.HandleFunc("/get/", s.webGet)
+	http.HandleFunc("/set/", s.webSet)
 	log.Printf("Starting web server on port %d...", *webport)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *webport), nil))
 }
